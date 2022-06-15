d1be09735f16c2a4c8e07577c1602c45fce3b1f2
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-15 18:27:20 -0400

    MediaPlayer convertido a una clase

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,42 +1,37 @@
-function MediaPlayer(config) {
-  this.media = config.el;
-  this.plugins = config.plugins || [];
+class MediaPlayer {
+  constructor(config) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
 
-  this._initPlugins();
-}
-
-MediaPlayer.prototype._initPlugins = function () {
-  this.plugins.forEach((plugin) => {
-    plugin.run(this);
-  });
-};
-
-MediaPlayer.prototype.play = function () {
-  this.media.play();
-};
-
-MediaPlayer.prototype.pause = function () {
-  this.media.pause();
-};
-
-MediaPlayer.prototype.mute = function () {
-  this.media.muted = true;
-};
-
-MediaPlayer.prototype.unmute = function () {
-  this.media.muted = false;
-};
-
-MediaPlayer.prototype.togglePlay = function () {
-  if (this.media.paused) {
-    this.play();
-  } else {
-    this.pause();
+    this._initPlugins();
   }
-};
-
-MediaPlayer.prototype.toggleMute = function () {
-  this.media.muted = !this.media.muted;
-};
+  _initPlugins() {
+    this.plugins.forEach((plugin) => {
+      plugin.run(this);
+    });
+  }
+  play() {
+    this.media.play();
+  }
+  pause() {
+    this.media.pause();
+  }
+  mute() {
+    this.media.muted = true;
+  }
+  unmute() {
+    this.media.muted = false;
+  }
+  togglePlay() {
+    if (this.media.paused) {
+      this.play();
+    } else {
+      this.pause();
+    }
+  }
+  toggleMute() {
+    this.media.muted = !this.media.muted;
+  }
+}
 
 export default MediaPlayer;
