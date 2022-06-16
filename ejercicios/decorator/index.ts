bdcc59f4b7b0d0460b01564f50bbcfcfecd49fa5
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-16 18:10:06 -0400

    Implementando el patrón de diseño Decorator

---
+++ ejercicios/decorator/index.ts
@@ -0,0 +1,61 @@
+class Field {
+  errors: string[];
+  input: HTMLInputElement;
+
+  constructor(input: HTMLInputElement) {
+    this.input = input;
+    this.errors = [];
+
+    let errorMessage = document.createElement("p");
+    errorMessage.className = "text-danger";
+    this.input.parentNode!.insertBefore(
+      errorMessage,
+      this.input.nextElementSibling
+    );
+
+    this.input.addEventListener("input", () => {
+      this.errors = [];
+      this.validate();
+      errorMessage.innerText = this.errors[0] || "";
+    });
+  }
+
+  validate() {}
+}
+
+function RequiredFieldDecorator(field: Field): Field {
+  let validate = field.validate;
+
+  field.validate = function () {
+    validate();
+
+    let value = field.input.value;
+
+    if (!value) {
+      field.errors.push("Requerido");
+    }
+  };
+
+  return field;
+}
+
+function EmailFieldDecorator(field: Field): Field {
+  let validate = field.validate;
+
+  field.validate = function () {
+    validate();
+
+    let value = field.input.value;
+
+    if (value.indexOf("@") === -1) {
+      console.log("Email invalido");
+      field.errors.push("Debe ser un email valido");
+    }
+  };
+
+  return field;
+}
+
+let field = new Field(document.querySelector("#email")!);
+field = RequiredFieldDecorator(field);
+EmailFieldDecorator(field);
